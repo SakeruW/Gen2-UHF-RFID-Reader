b46622f4356bdeea6562d37842cc1bae9af8f819
SakeruW <wangsakeru@gmail.com>
2023-01-18 15:04:48 +0000

    remove: unused files on rpi

--- WSL_Launcher.bat
+++
@@ -1,3 +0,0 @@
-FOR /F "tokens=1" %%i IN ('usbipd wsl list ^| findstr /c:"LimeSDR-USB"') DO SET VARIABLE=%%i
-
-wt -p "Ubuntu-20.04" ; new-tab -p "Command Prompt" cmd /k "usbipd wsl list & usbipd wsl attach --busid %VARIABLE%"
